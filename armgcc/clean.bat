a1c175cfbb47f33c9b112b82469791732fc89433
ISPIRIT <961512131@qq.com>
2018-05-30 03:15:33 +0800

    remove directory from remote repository

--- armgcc/clean.bat
+++
@@ -1,3 +0,0 @@
-RD /s /Q sdram_debug sdram_release CMakeFiles
-DEL /s /Q /F Makefile cmake_install.cmake CMakeCache.txt
-pause
--- mdk/sdram_debug/startup_mimxrt1052.d
+++
@@ -1,1 +0,0 @@
-sdram_debug\startup_mimxrt1052.o: ..\..\..\..\..\devices\MIMXRT1052\arm\startup_MIMXRT1052.s
